335d0c4c678d976df32867e9a471ebea4a21edcf
Por-creater <6714951007@rbru.ac.th>
2026-03-11 15:32:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pdfplumber
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCWJ4EfPOYhFdHRrrEx7RHeSq51y8lUzHc")
+genai.configure(api_key=st.secrets["GAIzaSyCWJ4EfPOYhFdHRrrEx7RHeSq51y8lUzHc"])
 
 st.title("AI PDF Summarizer")
 
@@ -10,7 +10,7 @@ uploaded_file = st.file_uploader("Upload PDF", type="pdf")
 
 if uploaded_file:
 
-    text = ""   # ต้องมีบรรทัดนี้ก่อน
+    text = ""
 
     with pdfplumber.open(uploaded_file) as pdf:
         for page in pdf.pages:
@@ -22,7 +22,7 @@ if uploaded_file:
 
     if st.button("Summarize"):
 
-        model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
         response = model.generate_content(
             f"Summarize this document:\n{text}"
